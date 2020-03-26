7a704d2c098a9a3c54ee4e6f805987f169d05470
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-26 09:13:00 -0500

    applied built in styles, and set up save button clicking. It now pulls the val of the textarea on the same row

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ var nextMoment;
 var workHoursStart;
 var workHoursEnd;
 
-var day = [];
+var day;
 var interval = 3600;    // in seconds  3600 = 1hr
 
 
@@ -20,16 +20,17 @@ var interval = 3600;    // in seconds  3600 = 1hr
 ####  Functions  ########################
 #######################################*/
 function generateTimeBlock(timeObj){
-    var curBlock = $('<div class="timeBlock row">')
+    var curBlock = $('<div id="block'+timeObj.index+'" class="timeBlock row" data-dayObj="'+timeObj.index+'">')
     
     // create each section of the time block, along with class info and ID
-    var timeBlockTime = $('<div class="col-2 timeBlockTime bg-primary text-center d-flex align-items-center justify-content-center">');
-    var timeBlockComment = $('<div class="col-8 timeBlockComment bg-secondary">');
-    var timeBlockSave = $('<div class="col-2 timeBlockSave bg-danger">');
+    var timeBlockTime = $('<div class="col-2 hour text-center">');
+    var timeBlockComment = $('<textarea class="col-8 timeBlockComment">');
+    var timeBlockSave = $('<div class="col-2 saveBtn">');
 
     //fill content
-    console.log(timeObj.time)
-    timeBlockTime.append($('<h4>'+moment(timeObj.time).format('LT')+"</h4>"));
+    timeBlockTime.text(moment(timeObj.time).format('LT'));
+    timeBlockComment.text(timeObj.comment);
+    timeBlockSave.append($('<i class="far fa-save"></i>'));
 
     // append each block to the curblockDiv
     curBlock.append(timeBlockTime);
@@ -39,6 +40,7 @@ function generateTimeBlock(timeObj){
     //return the completed timeBlock Div
     return curBlock;
 }
+
 
 function printTimeBlock(){
     // clear the timeBlock element
@@ -52,11 +54,36 @@ function printTimeBlock(){
     //console.log(day[0].time.format('LT'));
 }
 
+
+function colorTimeBlocks(){
+    for (var i=0; i<day.length; i++){
+        //get current block associated with day array
+        var curBlock = $("#block"+i);
+
+        if(day[i].time < curMoment){
+            if(day[i].time.clone().add(interval,'s') < curMoment){
+            curBlock.removeClass("present future")
+            curBlock.addClass("past");
+            }
+            else{
+                curBlock.removeClass("past future")
+                curBlock.addClass("present");
+            }
+        }
+        else{
+            curBlock.removeClass("past present")
+            curBlock.addClass("future");
+        }
+    }
+}
+
+
 function update(){
     //update the moment to get the current time
     curMoment = moment();
     // write the day to the DOM
-    writtenDayDisplay.text(curMoment);
+    writtenDayDisplay.text(curMoment.format('MMMM Do YYYY, h:mm:ss a'));
+    colorTimeBlocks();
 
     // if(nextMoment==undefined || curMoment > nextMoment){
     //     nextMoment = curMoment.add(1,"seconds");
@@ -65,19 +92,29 @@ function update(){
     // }
 }
 
+
 function constructDay(){
+    //try to pull from local storage
+    day = localStorage.getItem("day");
+    // if success, we're done here
+    if (day != undefined){return}
+    
+
+    //otherwise generate a new day setup
+    day=[];
     let time = moment().startOf('day');
     let endTime = moment().endOf('day');
+    let i=0;
     while(time < endTime){
         day.push({
             time: time.clone(),
-            comment:""
+            comment:"",
+            index:i
         });
-
+        i++;
         time.add(interval, "seconds");
     }
 }
-
 
 
 
@@ -91,7 +128,12 @@ constructDay();
 
 printTimeBlock();
 
-// update();
-// updateInterval = setInterval(update, 1000);
+ update();
+ updateInterval = setInterval(update, 1000);
+
+ $(".saveBtn").on("click",function (){
+    var message = $(this).parent().find(".timeBlockComment").val();
+    console.log(message);
+ })
 
 
