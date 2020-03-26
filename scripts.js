19c285c1db1c1f54a0ab618c5ed0e657da1ff28e
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-26 12:03:50 -0500

    implemented the ability to save the day array to local storage. Also, now converting the string 'time' elements back to moments when pulling from the local storage

--- scripts.js
+++ scripts.js
@@ -19,9 +19,11 @@ var interval = 3600;    // in seconds  3600 = 1hr
 /*#######################################
 ####  Functions  ########################
 #######################################*/
+
+// generate a fully stylized, fleshed out time block for the object passed
 function generateTimeBlock(timeObj){
     var curBlock = $('<div id="block'+timeObj.index+'" class="timeBlock row" data-dayObj="'+timeObj.index+'">')
-    
+    console.log(timeObj);
     // create each section of the time block, along with class info and ID
     var timeBlockTime = $('<div class="col-2 hour text-center">');
     var timeBlockComment = $('<textarea class="col-8 timeBlockComment">');
@@ -42,6 +44,7 @@ function generateTimeBlock(timeObj){
 }
 
 
+// loop through the day array.  append each as a generated timeblock to the DOM
 function printTimeBlock(){
     // clear the timeBlock element
     timeBlockDisplay.empty();
@@ -55,21 +58,27 @@ function printTimeBlock(){
 }
 
 
+// Iterates through the day object, gets index to pair with timeBlock, and colors as past, present, or future
 function colorTimeBlocks(){
+    //loop through every block that was displayed to the screen
     for (var i=0; i<day.length; i++){
-        //get current block associated with day array
+        //get current block associated with the timeObject in the day array
         var curBlock = $("#block"+i);
 
+        // setting past if it is lower than the current time
         if(day[i].time < curMoment){
+            // if time < now also time+interval < now : past time block
             if(day[i].time.clone().add(interval,'s') < curMoment){
-            curBlock.removeClass("present future")
-            curBlock.addClass("past");
+                curBlock.removeClass("present future")
+                curBlock.addClass("past");
             }
+            //otherwise, time < now , but time+interval > now : present time block
             else{
                 curBlock.removeClass("past future")
                 curBlock.addClass("present");
             }
         }
+        // time > now : future time block
         else{
             curBlock.removeClass("past present")
             curBlock.addClass("future");
@@ -83,8 +92,10 @@ function update(){
     curMoment = moment();
     // write the day to the DOM
     writtenDayDisplay.text(curMoment.format('MMMM Do YYYY, h:mm:ss a'));
+    //apply block color classes for past, present, and future
     colorTimeBlocks();
 
+    // method for updating other parts every longer interval - decided against doing this, but left for aditional features if needed later
     // if(nextMoment==undefined || curMoment > nextMoment){
     //     nextMoment = curMoment.add(1,"seconds");
     //     console.log("next update at "+nextMoment )
@@ -93,12 +104,18 @@ function update(){
 }
 
 
+//pulls the Day from local storage if it exists, else create a new array of objects
 function constructDay(){
     //try to pull from local storage
-    day = localStorage.getItem("day");
-    // if success, we're done here
-    if (day != undefined){return}
-    
+    day = JSON.parse(localStorage.getItem("day"));
+
+    // if success, reformat strings to moment (stringify simplified them to strings)
+    if (day != undefined){
+        for (var j =0; j<day.length; j++){
+            day[j].time = moment(day[j].time.split("T")[1], ["hh:mm:ss.SSSZ"])
+        }
+        return;
+    }
 
     //otherwise generate a new day setup
     day=[];
@@ -122,18 +139,32 @@ function constructDay(){
 ####  Main  #############################
 #######################################*/
 
-workHoursStart = 8;
-workHoursEnd = 17;
+// hardcoding work hours for now...
+workHoursStart = moment("08:00:00AM", ["hh:mm:ssA"]);
+workHoursEnd = moment("05:00:00PM", ["hh:mm:ssA"]);
+
+//get the day array of objects, wether it exists or not
 constructDay();
 
+//display all time blocks
 printTimeBlock();
 
- update();
- updateInterval = setInterval(update, 1000);
+//run the first update immediately, then ever 1 second after
+update();
+updateInterval = setInterval(update, 1000);
 
+//set up the save buttons
  $(".saveBtn").on("click",function (){
+     //get required ID and Message from the selected save row
+    var dayIndex = $(this).parent().attr("data-dayObj");
     var message = $(this).parent().find(".timeBlockComment").val();
-    console.log(message);
+
+    // update the object associated only with this row
+    day[dayIndex].comment = message;
+
+    //save the data
+    localStorage.setItem('day', JSON.stringify(day));
+    //console.log(dayIndex +" : "+ message);
  })
 
 
